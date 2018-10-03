ccf30718e627bb7ce41b319900cf290bca4dd7c8
chrislucas <christoffer.luccas@gmail.com>
2018-10-03 19:20:32 -0300

    adicionando conjunto de locais para teste

--- ClusteringLocation/src/solution/test/TestGroupLocationWithLimitSize.java
+++ ClusteringLocation/src/solution/test/TestGroupLocationWithLimitSize.java
@@ -193,10 +193,116 @@ public class TestGroupLocationWithLimitSize {
             )
             // # FIM - 00
         }
+        , {
+            p
+            , q
+            , u
+            // Melhor ponto 'U'
+            , new Location(3
+                , new ArrayList<>(Arrays.asList(
+                    new Location(p, 100, 1000)
+                    , new Location(q, 120, 1500)
+                    , new Location(u, 80, 1000)
+                    )
+                )
+            )
+                    // Melhor ponto 'U'
+            , new Location(4
+                , new ArrayList<>(Arrays.asList(
+                         new Location(p, 120, 1000)
+                        , new Location(q, 120, 1500)
+                        , new Location(u, 80, 1000)
+                    )
+                )
+            )
+                    // melhor ponto 'p'
+            , new Location(5
+                , new ArrayList<>(Arrays.asList(
+                         new Location(p, 80, 1000)
+                        , new Location(q, 130, 1500)
+                        , new Location(u, 180, 1000)
+                    )
+                )
+            )
+                    // melhor ponto 'p'
+            , new Location(6
+                , new ArrayList<>(Arrays.asList(
+                          new Location(p, 80, 1200)
+                        , new Location(q, 130, 1500)
+                        , new Location(u, 180, 1000)
+                    )
+                )
+            )
+                // melhor ponto 'p'
+             , new Location(7
+                , new ArrayList<>(Arrays.asList(
+                          new Location(p, 75, 1200)
+                        , new Location(q, 130, 1500)
+                        , new Location(u, 180, 1000)
+                    )
+                )
+            )
+                    // melhor ponto 'p'
+            , new Location(8
+                , new ArrayList<>(Arrays.asList(
+                          new Location(p, 75, 1200)
+                        , new Location(q, 130, 1000)
+                        , new Location(u, 120, 800)
+                    )
+                )
+            )
+            // melhor ponto 'p'
+            , new Location(9
+                , new ArrayList<>(Arrays.asList(
+                          new Location(p, 75, 1200)
+                        , new Location(q, 130, 1000)
+                        , new Location(u, 120, 800)
+                    )
+                )
+            )
+            , // melhor ponto: 'q'
+                new Location(10
+                    , new ArrayList<>(Arrays.asList(
+                        new Location(p, 75, 1200)
+                        , new Location(q, 30, 1500)
+                        , new Location(u, 180, 1000)
+                    )
+                )
+            )
+
+        , // melhor ponto: 'q'
+            new Location(11
+                , new ArrayList<>(Arrays.asList(
+                          new Location(p, 75, 1200)
+                        , new Location(q, 20, 1000)
+                        , new Location(u, 180, 1000)
+                    )
+                )
+            )
+
+            , // melhor ponto: 'q' parecido com o ponto 8, porem mandar o ponto 9 para referencia 'p'  demora menos
+            new Location(12
+                    , new ArrayList<>(Arrays.asList(
+                          new Location(p, 75, 1000)
+                        , new Location(q, 20, 1000)
+                        , new Location(u, 180, 1000)
+                    )
+                )
+            )
+                    , // melhor ponto: 'q' parecido com o ponto 8, porem mandar o ponto 9 para referencia 'p'  demora menos
+            new Location(13
+                , new ArrayList<>(Arrays.asList(
+                        new Location(p, 75, 1000)
+                        , new Location(q, 300, 1000)
+                        , new Location(u, 180, 1000)
+                    )
+                )
+            )
+        }
     };
 
     private static void runTest() {
-        ArrayList<Location> locs = new ArrayList<>(Arrays.asList(locations[0]));
+        ArrayList<Location> locs = new ArrayList<>(Arrays.asList(locations[1]));
         Group group = new GroupLocationByDistanceAndTime(locs);
         group.apply();
         LinkedHashMap<Location, LinkedHashSet<Location>> graph = ((GroupLocationByDistanceAndTime) group).getGraph();
